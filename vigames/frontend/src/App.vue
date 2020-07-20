d453d968bc47b6817b133104ec8ee8f98dcb40fd
Algasenok <aleksandr.kozlyakov@yandex.ru>
2020-07-20 22:30:15 +0300

    Папка frontend перемещена в папку проекта (предыдущий коммит не сработал)

---
+++ vigames/frontend/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <header-component></header-component>
+    <breadcrumbs-component></breadcrumbs-component>
+    <game-single-page></game-single-page>
+    <footer-component></footer-component>
+  </div>
+</template>
+
+<script>
+  import HeaderComponent from "@/components/HeaderComponent";
+  import BreadcrumbsComponent from "@/components/BreadcrumbsComponent";
+  import GameSinglePage from "@/components/pages/GameSinglePage";
+  import FooterComponent from "@/components/FooterComponent";
+
+  export default {
+    name: 'App',
+    components: {
+      HeaderComponent,
+      BreadcrumbsComponent,
+      GameSinglePage,
+      FooterComponent
+    }
+}
+</script>
+
+<style>
+  #app {
+    font-family: Roboto;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #000;
+    font-style: normal;
+    font-size: 16px;
+    font-weight: normal;
+  }
+
+  body {
+    margin: 0;
+  }
+  /* fonts */
+  /* roboto-regular - latin_cyrillic */
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Roboto'), local('Roboto-Regular'),
+    url('/fonts/roboto-v20-latin_cyrillic-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+    url('/fonts/roboto-v20-latin_cyrillic-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+  }
+  /* roboto-700 - latin_cyrillic */
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    src: local('Roboto Bold'), local('Roboto-Bold'),
+    url('/fonts/roboto-v20-latin_cyrillic-700.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+    url('/fonts/roboto-v20-latin_cyrillic-700.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+  }
+
+  /* fonts end */
+</style>
---
+++ vigames/frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,130 @@
+<template>
+    <header class="header">
+        <div class="wrapper">
+            <div class="header__wrapper">
+                <div class="header__logo">
+                    <a href="#!">
+                        <img src="@/assets/img/logo.svg" alt="Vigrom" class="header__logo-pic">
+                    </a>
+                </div>
+
+                <nav class="header__nav">
+                    <ul class="header__nav-list">
+                        <li class="header__item">
+                            <a href="#!" class="header__link">Магазин</a>
+                        </li>
+                        <li class="header__item">
+                            <a href="#!" class="header__link">Новости</a>
+                        </li>
+                        <li class="header__item">
+                            <a href="#!" class="header__link">Библиотека</a>
+                        </li>
+                        <li class="header__item">
+                            <a href="#!" class="header__link">Комьюнити</a>
+                        </li>
+                        <li class="header__item">
+                            <a href="#!" class="header__link">Справка</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div class="header__icons">
+                    <div class="header__icon">
+                        <a href="#!" class="header__icon-link">
+                            <img src="@/assets/img/search-icon.png" alt="Search" class="header__icon-pic">
+                        </a>
+                    </div>
+                    <div class="header__icon">
+                        <a href="#!" class="header__icon-link">
+                            <img src="@/assets/img/cart-icon.png" alt="Cart" class="header__icon-pic">
+                        </a>
+                    </div>
+                    <div class="header__icon">
+                        <a href="#!" class="header__icon-link">
+                            <img src="@/assets/img/person-icon.png" alt="Person" class="header__icon-pic">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "HeaderComponent"
+    }
+</script>
+
+<style scoped>
+    .header {
+        background-color: #848484;
+        margin-bottom: 74px;
+    }
+
+    .wrapper {
+        margin: 0 auto;
+        max-width: 1124px;
+    }
+
+    .header__wrapper {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+
+    .header__logo {
+        margin: 20px 0;
+    }
+
+    .header__nav-list {
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        margin-bottom: 31px;
+        margin-top: 33px;
+    }
+
+    .header__item {
+        margin-right: 31px;
+    }
+
+    .header__item:last-child {
+        margin-right: 0;
+    }
+
+    .header__link {
+        font-weight: bold;
+        font-size: 14px;
+        line-height: 16px;
+        color: #000;
+        text-decoration: none;
+    }
+
+    .header__link:hover, .header__link:active, .header__link:focus {
+        opacity: .75;
+    }
+
+    .header__nav {
+        margin-left: 119px;
+    }
+
+    .header__icons {
+        margin: 28px 0 28px 348px;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .header__icon {
+        margin-right: 25px;
+    }
+
+    .header__icon-link:hover, .header__icon-link:active, .header__icon-link:focus {
+        opacity: .65;
+    }
+
+    .header__icon:last-child {
+        margin-right: 0;
+    }
+
+</style>
