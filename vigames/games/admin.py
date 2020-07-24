6da5b682b2630918bf29594658a3c2ee88e03e6a
Algasenok <aleksandr.kozlyakov@yandex.ru>
2020-07-24 15:34:17 +0300

    Убрана "роль" из админки, т.к. модель закомментирована временно

--- vigames/games/admin.py
+++ vigames/games/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Category, Media, Account, Role, Posts
+from .models import Category, Media, Account, Posts
 
 admin.site.register(Posts)
 admin.site.register(Category)
 admin.site.register(Account)
-admin.site.register(Role)
+#admin.site.register(Role)
 admin.site.register(Media)
