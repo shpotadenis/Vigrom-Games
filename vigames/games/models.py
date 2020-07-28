548d54d7b05760d459037ea68e632502a3270b2f
Kate <skv.studies@gmail.com>
2020-07-28 13:15:24 +0300

    Исправлено добавление и просмотр игр. Добавлено редактирование и удаление игр.

--- vigames/games/models.py
+++ vigames/games/models.py
@@ -39,21 +39,6 @@ class Media(models.Model):
         verbose_name = "Изображение"
         verbose_name_plural = "Изображения"
 
-class Game(models.Model):
-    #players = models.ManyToManyField(Account, null=True)
-    title = models.CharField(max_length=32, default="")
-    url = models.CharField(max_length=250, null=True) #по идее можно выпилить?
-    short_description = models.CharField(max_length=250, default="")
-    image = models.ImageField('Изображение игры', upload_to='img/%Y/%m', null=True)
-    gameplay_video_link = models.CharField(max_length=250, null=True)
-    release_status = models.BooleanField(default=False)
-    price = models.IntegerField(default=0)
-    screenshots = models.ImageField(upload_to='img/%Y/%m', null=True)
-    #uploads
-    description = models.TextField(default="")
-    genre = models.CharField(max_length=50, default="")
-    tags = models.CharField(max_length=50, default="")
-
 
 class Account(models.Model):
     # Аккаунт пользователя
@@ -67,7 +52,7 @@ class Account(models.Model):
         phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
         phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
     '''
-    name = models.CharField("Имя", max_length=30, null=True)
+    name = models.CharField("Имя", max_length=30, null=True, default="")
     lastname = models.CharField("Фамилия", max_length=30, null=True)
     date_birth = models.DateField("Дата рожения: ДД.ММ.ГГГГ", max_length=10, null=True)
     city = models.CharField("Город", max_length=50, null=True)
@@ -78,17 +63,34 @@ class Account(models.Model):
     is_administrator = models.BooleanField(default=False, null=True)
     bank_cаrd = models.CharField("Номер карты", max_length=20, null=True)
     foto = models.ImageField("Аватар", upload_to="img/%Y/%m", null=True)
-    developed_games = models.ForeignKey(Game, on_delete=models.PROTECT,
-                                        related_name="developed_games", null=True)
-    bought_games = models.ManyToManyField(Game, related_name="bought_games", blank=True)
+    #developed_games = models.ForeignKey(Game, on_delete=models.PROTECT,
+    #                                   related_name="developed_games", null=True)
+    #bought_games = models.ManyToManyField(Game, related_name="bought_games", blank=True)
     #Добавить список игр - скорее всего связь многие ко многим
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     class Meta:
         verbose_name = "Аккаунт"
         verbose_name_plural = "Аккаунты"
+
+class Game(models.Model):
+    players = models.ManyToManyField(Account, blank=True, related_name="players")
+    author = models.ForeignKey(Account, on_delete=models.PROTECT,
+                                        related_name="author", null=True)
+    title = models.CharField(max_length=32, default="")
+    url = models.CharField(max_length=250, null=True) #по идее можно выпилить?
+    short_description = models.CharField(max_length=250, default="")
+    image = models.ImageField('Изображение игры', upload_to='img/%Y/%m', null=True)
+    gameplay_video_link = models.CharField(max_length=250, null=True)
+    release_status = models.BooleanField(default=False)
+    price = models.IntegerField(default=0)
+    screenshots = models.ImageField(upload_to='img/%Y/%m', null=True)
+    #uploads
+    description = models.TextField(default="")
+    genre = models.CharField(max_length=50, default="")
+    tags = models.CharField(max_length=50, default="")
 
 '''
 class Basket(models.Model):
