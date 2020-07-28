c0868313f35e93d1cec7f7e0d275e0d5d67e25ee
kurkutka <kurvvvvv5@gmail.com>
2020-07-28 11:40:40 +0300

    scripts_new

--- vigames/games/scripts.py
+++ vigames/games/scripts.py
@@ -5,7 +5,6 @@ class Search:
     def search(self, reqst, games):
         end_spis = {}  # словарь с рейтингом названий
         right_letters = 0
-        retturn = []
         for elem in games:
             elem.lower()
             for el in range(len(elem)):
@@ -14,8 +13,7 @@ class Search:
                         right_letters += 1
             end_spis[elem] = right_letters
             right_letters = 0
-        for numm in end_spis:
-            if numm >
+        return s
 
 
 s = []
