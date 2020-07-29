83dc6464d1c2ed0604e3a2d8142b282c2e5ebc83
VladislavTrifonov <vtrifonov548@gmail.com>
2020-07-29 15:02:35 +0300

    Обновление API (пользователи)

--- vigames/frontend/src/api/modules/user.js
+++ vigames/frontend/src/api/modules/user.js
@@ -7,12 +7,19 @@ const register_endpoint = '/auth/users'
 export default {
     login(credentials)
     {
-        return instance.post(login_endpoint, credentials)
+        return instance.post(login_endpoint, {
+            'username': credentials.login,
+            'password': credentials.password
+        })
     },
 
     register(credentials)
     {
-        return instance.post(register_endpoint, credentials)
+        return instance.post(register_endpoint, {
+            'username': credentials.login,
+            'password': credentials.password,
+            'email': credentials.email
+        })
     }
 
 }
