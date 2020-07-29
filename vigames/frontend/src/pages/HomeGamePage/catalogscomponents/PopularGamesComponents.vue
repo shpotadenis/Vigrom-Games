b61b885784e08569f18b06252fd1182130dda6b2
Lagumir <Legals92@mail.ru>
2020-07-29 15:49:00 +0300

    HomePage

--- vigames/frontend/src/pages/HomeGamePage/catalogscomponents/PopularGamesComponents.vue
+++ vigames/frontend/src/pages/HomeGamePage/catalogscomponents/PopularGamesComponents.vue
@@ -2,17 +2,20 @@
 <div id="wrappen">
 	<div class="AllItem">
 		<div class="ItemImage">
-			<img src="@/assets/img/pic1.png" width="1000" height="390">
+
+			<img v-bind:src="require('@/assets/' + Game_Popular.img) " width="1000" height="390">
+
 		</div>
 		<div class="Info">
 			<div class="ItemInfoGame">
-				<p class="NameGame">NameGame</p>
-				<p class="DiscriptionGame">Мультиплатформенная компьютерная игра в жанре action/RPG, разработанная
-польской студией CD Projekt RED</p>
-	
+				<p class="NameGame">{{Game_Popular.name}}
+
+				</p>
+
+				<p class="DiscriptionGame">{{Game_Popular.discription}}</p>
+				<p class="LinkGame"><a href="#!" >Подробнее <img src="@/assets/strelka.svg" class="ImageArrow"></a></p>
+
 			</div>
-			
-				
 		</div>
 	</div>
 </div>
@@ -22,10 +25,20 @@
 
 
 export default {
-  name: 'PopularGamesComponents',
-  components: {
-    
-  }
+name: 'PopularGamesComponents',
+components: {
+
+},
+props:{
+	Game_Popular:{
+		type:Object,
+		default(){
+			return{}
+		}
+	}
+}
+
+
 }
 </script>
 
@@ -34,6 +47,10 @@ export default {
 position:relative;
  margin-top:48px;
 }
+.ItemImage{
+	position:relative;
+}
+
 .Info{
 	position:relative;
 	width:1000px;;
@@ -42,6 +59,7 @@ position:relative;
 	background-color: #2D2D42;
 }
 .NameGame{
+	width:200px;
 	color:#FFF;
 	font-style: normal;
 	font-weight: 800;
@@ -50,23 +68,40 @@ position:relative;
 	text-transform:uppercase;
 	position: absolute;
 	padding-bottom:2px;
-	padding-right:40px;
 	height:28px;
     top: 35%;
     left: 4%;
-    border-right:1px solid #fff;
+
 }
 .DiscriptionGame{
 	position:absolute;
-	top: 30%;
-    left: 27%;
+	top: 35%;
+    left: 20%;
 	width:470px;
+	margin-left:30px;
 	color:#fff;
 	font-style: normal;
 	font-weight: normal;
 	font-size: 12px;
 	line-height: 16px;
+	padding-left:20px;
+	border-left:1px solid #fff;
 
+}
+.LinkGame{
+	position:absolute;
+	top: 40%;
+    left: 85%;
+
+
+}
+.LinkGame a{
+	color:#fff;
+	text-decoration:none;
+	height:50px;
+}
+.ImageArrow{
+	padding-left:10px;
 }
 
 </style>
