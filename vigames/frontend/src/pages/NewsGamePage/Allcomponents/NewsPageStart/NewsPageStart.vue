8a89e0655e86dbd90414f7ad536d8ecb5a16c01b
Lagumir <Legals92@mail.ru>
2020-07-31 01:04:41 +0300

    NewsFeed add

---
+++ vigames/frontend/src/pages/NewsGamePage/Allcomponents/NewsPageStart/NewsPageStart.vue
@@ -0,0 +1,141 @@
+<template>
+<div>
+  <div class="NewsSectionAndSearch">
+    <div class="Block1">
+      <p class="NewsSection">Новостной раздел</p>
+    </div>
+    <div class="Block2">
+      <img src="@/assets/img/search-icon.png" class="ImageSeach">
+      <input
+      type="text"
+      placeholder="Поиск..."
+      class="SearchNews"
+      >
+      <img src="@/assets/img/block.svg" class="SearchImage">
+    </div>
+    </div>
+    <div class="BlockBaner">
+      <div class="TextBaner">
+        <p class="DateBaner">Сегодня</p>
+        <p class="NameBaner">МЫ ОПУСКАЕМ ПЛАНКУ ВХОДА</p>
+        <p class="UnderNameBaner">Унитарное государство, однако, доказывает системный политический процесс в современной России</p>
+        <button class="ButtonBaner">Подробнее <img src="@/assets/img/Vector.png"></button>
+      </div>
+      <div>
+        <img src="@/assets/img/902d1.svg" class="ImageBest">
+      </div>
+    </div>
+</div>
+</template>
+
+
+<script>
+export default{
+  name:"NewsPageStart",
+  components:{
+
+  },
+  props:{},
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.NewsSectionAndSearch{
+display:flex;
+justify-content:space-between;
+}
+.NewsSection{
+position:relative;
+top:10px;
+font-style: normal;
+font-weight: 800;
+font-size: 14px;
+line-height: 19px;
+text-transform:uppercase;
+color: #FFFFFF;
+}
+
+.SearchNews{
+background-color:#212130;
+border:none;
+position:relative;
+left:30px;
+}
+.ImageSeach{
+position:relative;
+top:-5px;
+left:20px;
+}
+.SearchImage{
+position:relative;
+top:9px;
+
+
+}
+.BlockBaner{
+display:flex;
+justify-content:space-between;
+margin-top:61px;
+width:1110px;
+height:294px;
+background: #2D2D42;
+border-radius: 10px;
+}
+.DateBaner{
+position:relative;
+top:40px;
+left:40px;
+text-transform:uppercase;
+font-style: normal;
+font-weight: normal;
+font-size: 14px;
+line-height: 19px;
+color: #7A7A83;
+}
+.NameBaner{
+position:relative;
+top:104px;
+left:40px;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 28px;
+color: #FFFFFF;
+}
+.UnderNameBaner{
+position:relative;
+top:110px;
+left:40px;
+width:411px;
+font-style: normal;
+font-weight: normal;
+font-size: 14px;
+line-height: 19px;
+color: #7A7A83;
+}
+.ButtonBaner{
+position:relative;
+top:136px;
+left:40px;
+width: 139px;
+height: 38px;
+background: #CE6796;
+border-radius: 6px;
+border:none;
+font-style: normal;
+font-weight: 800;
+font-size: 14px;
+line-height: 19px;
+color: #FFFFFF;
+}
+.ImageBest{
+position:relative;
+top:28px;
+right:95px;
+}
+</style>
--- vigames/frontend/src/pages/NewsGamePage/NewsPage.vue
+++ vigames/frontend/src/pages/NewsGamePage/NewsPage.vue
@@ -1,9 +1,32 @@
 <template>
 <div class="container">
 	<div class="col">
-		<div class="wrappen">
-			<footer-component></footer-component>
-		</div>
+		<div class="NewsWrappen">
+			<news-page-start/>
+			<div class="SelectedArticlesText">
+				<p class="SelectedArticlesTitle">ИЗБРАННЫЕ СТАТЬИ</p>
+				<div class="SelectedArticlesComp">
+					<selected-articles
+						v-for="articles in Selected"
+						:key="articles.id"
+						:Selected_News="articles"
+					/>
+				</div>
+			</div>
+			<div class="NewsFeedText">
+				<p class="NewsFeedTitle">НОВОСТНАЯ ЛЕНТА</p>
+				<div class="NewsFeed">
+					<news-feed
+						v-for="news in Feeds"
+						:key="news.id"
+						:News_Feed="news"
+					/>
+				</div>
+			</div>
+			<div class="foot">
+				<footer-component></footer-component>
+			</div>
+			</div>
 	</div>
 </div>
 </template>
@@ -11,30 +34,109 @@
 
 <script>
 
+import NewsPageStart from "./Allcomponents/NewsPageStart/NewsPageStart.vue"
+import SelectedArticles from "./Allcomponents/NewsPageStart/SelectedArticles.vue"
+import NewsFeed from "./Allcomponents/NewsPageStart/NewsFeed.vue"
+import FooterComponent from "../../components/FooterComponent/index.vue"
 
 
-	import FooterComponent from "../../components/FooterComponent/index.vue"
-
-
-	export default{
-		name:"NewsPage",
-		components:{
-
-			FooterComponent
-		},
-		props:{},
-		data(){
-			return{
-
-			}
-		}
-	}
+export default{
+  name:"NewsPage",
+  components:{
+    NewsPageStart,
+		SelectedArticles,
+		NewsFeed,
+    FooterComponent
+  },
+  props:{},
+  data(){
+    return{
+			Selected:[
+			{
+				date:"Сегодня",
+				name:"Новое оборудование на серверах Vigrom",
+				id:"1"
+			},
+			{
+				date:"Сегодня",
+				name:"Новое оборудование на серверах Vigrom",
+				id:"2"
+			},
+			{
+				date:"Сегодня",
+				name:"Новое оборудование на серверах Vigrom",
+				id:"3"
+			},
+			{
+				date:"Сегодня",
+				name:"Новое оборудование на серверах Vigrom",
+				id:"4"
+			},
+			],
+			Feeds:[
+			{
+				id:"1",
+				image:"image.svg",
+				name:"Новое оборудование на серверах Vigrom",
+				undername:"Как известно, политический процесс в современной России неравномерен. Типология средств..."
+			},
+			{
+				id:"2",
+				image:"image.svg",
+				name:"Новое оборудование на серверах Vigrom",
+				undername:"Как известно, политический процесс в современной России неравномерен. Типология средств..."
+			},
+			{
+				id:"3",
+				image:"image.svg",
+				name:"Новое оборудование на серверах Vigrom",
+				undername:"Как известно, политический процесс в современной России неравномерен. Типология средств..."
+			},
+			{
+				id:"4",
+				image:"image.svg",
+				name:"Новое оборудование на серверах Vigrom",
+				undername:"Как известно, политический процесс в современной России неравномерен. Типология средств..."
+			},
+			]
+    }
+  }
+}
 </script>
 
 <style scoped>
-.wrappen{
-	width:1100px;
-	margin:0 auto;
-}
 
+.NewsWrappen{
+width:1110px;
+margin:auto;
+}
+.SelectedArticlesText{
+margin-top:100px;
+}
+.SelectedArticlesTitle{
+font-style: normal;
+font-weight: 800;
+font-size: 14px;
+line-height: 19px;
+text-transform:uppercase;
+color: #FFFFFF;
+}
+.SelectedArticlesComp{
+display:flex;
+justify-content:space-between;
+}
+.NewsFeedText{
+margin-top:100px;
+}
+.NewsFeedTitle{
+font-style: normal;
+font-weight: 800;
+font-size: 14px;
+line-height: 19px;
+text-transform:uppercase;
+color: #FFFFFF;
+}
+.foot{
+margin-top:150px;	
+}
 </style>
