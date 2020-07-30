d5db187db3de2a7ce9c46485d206d215cb545421
Белошапко Кристина <uni.3456@outlook.com>
2020-07-30 18:01:57 +0300

    Подключена страница входа

---
+++ vigames/frontend/src/pages/SignInPage/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Здесь будет страничка
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SignInPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
