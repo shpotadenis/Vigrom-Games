823c1ba404eb2b9a9411c669ca1c3b0da2e97719
Белошапко Кристина <uni.3456@outlook.com>
2020-07-30 18:00:59 +0300

    Страница регистрации

--- vigames/frontend/src/pages/LoginPage/index.js
+++ vigames/frontend/src/pages/LoginPage/index.js
@@ -4,6 +4,36 @@ const data = {
 
 export default {
     name: "LoginPage",
+    data() {
+        return{
+        nonEye: false,
+            nonEyeconf: false,
+        type: 'password',
+            typeconf: 'password'
+        }
+    },
+    methods: {
+        hide(){
+            if (!this.nonEye){
+                this.nonEye = true;
+                this.type = 'text';
+            }
+            else{
+                this.nonEye = false;
+                this.type = 'password';
+            }
+        },
+        hideconf(){
+            if (!this.nonEyeconf){
+                this.nonEyeconf = true;
+                this.typeconf = 'text';
+            }
+            else{
+                this.nonEyeconf = false;
+                this.typeconf = 'password';
+            }
+        }
+    },
     components: {
 
     },
