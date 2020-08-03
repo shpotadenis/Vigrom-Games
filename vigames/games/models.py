a8ff4f3e5d6c69810623a75f88fd7de8b4211aa7
Kate <skv.studies@gmail.com>
2020-08-03 13:28:11 +0300

    Изменена модель Media.

--- vigames/games/models.py
+++ vigames/games/models.py
@@ -4,19 +4,6 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 
-class Media(models.Model):
-    title = models.CharField('Заголовок изображения', max_length=50)
-    img = models.ImageField("Изображение", upload_to="img/%Y/%m")
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = "Изображение"
-        verbose_name_plural = "Изображения"
-
-
 class Genre(models.Model):
     strategy = models.IntegerField(default=0)
     rpg = models.IntegerField(default=0)
@@ -91,6 +78,7 @@ class Category(models.Model):
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
+
 #в продакшене выпилить некоторые null=True (сейчас удобно тестировать с ними)
 class Game(models.Model):
     players = models.ManyToManyField(Account, blank=True, related_name="players")
@@ -102,7 +90,7 @@ class Game(models.Model):
     #url = models.CharField(max_length=250, null=True)  # по идее можно выпилить?
     file = models.FileField(null=True, upload_to=u'file/%Y/%m', default=None)
     short_description = models.CharField(max_length=250, default="")
-    image = models.ImageField('Изображение игры', upload_to='img/%Y/%m', null=True, default=None)
+    #image = models.ImageField('Изображение игры', upload_to='img/%Y/%m', null=True, default=None)
     gameplay_video_link = models.CharField(max_length=250, null=True, default=None)
     release_status = models.BooleanField(default=False)
     date_release = models.DateField(default=date.today)
@@ -216,3 +204,18 @@ class Rating(models.Model):
 class FAQ(models.Model):
     question = models.CharField(max_length=150)
     answer = models.TextField()
+
+
+class Media(models.Model):
+    title = models.CharField('Заголовок изображения', max_length=50)
+    img = models.ImageField("Изображение", upload_to="img/%Y/%m", null=True, default=None)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Изображение"
+        verbose_name_plural = "Изображения"
