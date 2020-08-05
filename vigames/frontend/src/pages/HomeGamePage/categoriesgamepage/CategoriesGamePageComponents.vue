2f7bd79e409785bac696a362674c648c5c990ae2
Lagumir <Legals92@mail.ru>
2020-08-05 17:11:26 +0300

    картинки у категорий

--- vigames/frontend/src/pages/HomeGamePage/categoriesgamepage/CategoriesGamePageComponents.vue
+++ vigames/frontend/src/pages/HomeGamePage/categoriesgamepage/CategoriesGamePageComponents.vue
@@ -4,27 +4,27 @@
   <a @click="isBtnClick = true">
   <div class="AllCategories">
     <div class="CategoriesGamePageComponent">
-      <img src="" class="ImageCategories">
+      <img src="@/style/img/flash-icon.svg" class="ImageCategories">
       <p class="TitleCategories">Экшен</p>
     </div>
     <div class="CategoriesGamePageComponent">
-      <img src="" class="ImageCategories">
+      <img src="@/style/img/cube-icon.svg" class="ImageCategories">
       <p class="TitleCategories">Симуляторы</p>
     </div>
     <div class="CategoriesGamePageComponent">
-      <img src="" class="ImageCategories">
+      <img src="@/style/img/map-icon.svg" class="ImageCategories">
       <p class="TitleCategories">Стратегии</p>
     </div>
     <div class="CategoriesGamePageComponent">
-      <img src="" class="ImageCategories">
+      <img src="@/style/img/smiling-face-outline.svg" class="ImageCategories">
       <p class="TitleCategories">Казуальные</p>
     </div>
     <div class="CategoriesGamePageComponent">
-      <img src="" class="ImageCategories">
+      <img src="@/style/img/globe-icon.svg" class="ImageCategories">
       <p class="TitleCategories">Приключения</p>
     </div>
     <div class="CategoriesGamePageComponent">
-      <img src="" class="ImageCategories">
+      <img src="@/style/img/shield-icon.svg" class="ImageCategories">
       <p class="TitleCategories">ММО</p>
     </div>
   </div>
