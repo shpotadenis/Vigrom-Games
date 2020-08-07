7c17c83920d213173ba544f14de06dc747e86b44
Alexander Sergeev <Hesss@users.noreply.github.com>
2020-08-07 12:24:36 +0300

    Добавил компонент pop-up "ММО"

---
+++ vigames/frontend/src/components/Pop-ups/TopCategory/IMO/index.js
@@ -0,0 +1,51 @@
+import GameInfoCard from "../../../../components/GameInfoCard/index.vue"
+
+export default {
+    name: "imo",
+
+    components:{
+      GameInfoCard
+    },
+    data(){
+      return{
+        GameInfoCard:[
+          {
+            id:"1",
+            name:"DANGER CREW",
+            undername1:"Danger Crew is a classic single player RPG set in the world of programming and hackers.",
+            image:"MaskGroup.svg",
+            genre:"Приключения",
+            undername2:"It's your first day of work as a lead engineer at Five Star Belts, the finest craftsmen belt company the city of Clayton has ever seen. Rumors have been spreading ever since a meeting with a nefarious vendor hit the calendars. The company might be in danger."
+          },
+          {
+            id:"2",
+            name:"DANGER CREW",
+            undername1:"Danger Crew is a classic single player RPG set in the world of programming and hackers.",
+            image:"MaskGroup.svg",
+            genre:"Ужасы"
+          },
+          {
+            id:"3",
+            name:"DANGER CREW",
+            undername1:"Danger Crew is a classic single player RPG set in the world of programming and hackers.",
+            image:"MaskGroup.svg",
+            genre:"Ужасы"
+          },
+          {
+            id:"4",
+            name:"DANGER CREW",
+            undername1:"Danger Crew is a classic single player RPG set in the world of programming and hackers.",
+            image:"MaskGroup.svg",
+            genre:"Ужасы"
+          },
+          {
+            id:"5",
+            name:"DANGER CREW",
+            undername1:"Danger Crew is a classic single player RPG set in the world of programming and hackers.",
+            image:"MaskGroup.svg",
+            genre:"Ужасы"
+          },
+        ]
+      }
+    }
+}
