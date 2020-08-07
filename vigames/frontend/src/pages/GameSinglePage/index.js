55694e8219d09af087c81286a13a4cfb2b0991e7
VladislavTrifonov <vtrifonov548@gmail.com>
2020-08-07 11:10:59 +0300

    Скачивание игры (API)

--- vigames/frontend/src/pages/GameSinglePage/index.js
+++ vigames/frontend/src/pages/GameSinglePage/index.js
@@ -98,11 +98,11 @@ export default {
                 const url = window.URL.createObjectURL(new Blob([response.data]))
                 const link = document.createElement('a')
                 link.href = url
-                link.setAttribute('download', 'file') //or any other extension
+                link.setAttribute('download', '') //or any other extension
                 document.body.appendChild(link)
                 link.click()
             }).catch(error => {
-                console.log(error)
+                console.log(error.response)
             })
         },
         addToWishlistClick() {
