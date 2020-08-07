e94d6fb9a8259539c560225e15a61dba499a5817
Alexander Sergeev <Hesss@users.noreply.github.com>
2020-08-07 12:21:08 +0300

    В компоненте жанров удалил подключения общего комп и подключил отдельные компоненты

--- vigames/frontend/src/pages/HomeGamePage/categoriesgamepage/CategoriesGamePageComponents.js
+++ vigames/frontend/src/pages/HomeGamePage/categoriesgamepage/CategoriesGamePageComponents.js
@@ -1,16 +1,27 @@
-import TopCategory from "../../../components/Pop-ups/TopCategory/index.vue"
-
+import Action from "../../../components/Pop-ups/TopCategory/Action/index.vue"
+import Simulators from "../../../components/Pop-ups/TopCategory/Simulators/index.vue"
+import Strategies from "../../../components/Pop-ups/TopCategory/Strategies/index.vue"
+import Casual from "../../../components/Pop-ups/TopCategory/Casual/index.vue"
+import Adventures from "../../../components/Pop-ups/TopCategory/Adventures/index.vue"
+import Imo from "../../../components/Pop-ups/TopCategory/IMO/index.vue"
 export default {
   name: 'CategoriesGamePageComponent',
   components: {
-    TopCategory
+    Action,
+    Simulators,
+    Strategies,
+    Casual,
+    Adventures,
+    Imo
   },
 data(){
   return {
-    TopCategory:{
-      genre: 'Test complite'
-    },
-    isBtnClick: false
+    isBtnClick_1: false,
+    isBtnClick_2: false,
+    isBtnClick_3: false,
+    isBtnClick_4: false,
+    isBtnClick_5: false,
+    isBtnClick_6: false
   }
 },
   props:{
