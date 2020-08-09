a789221e9571e647136b38447c5684858f569911
VladislavTrifonov <vtrifonov548@gmail.com>
2020-08-09 14:39:03 +0300

    Исправления для GameCard

--- vigames/frontend/src/components/GameCard/GameCard.vue
+++ vigames/frontend/src/components/GameCard/GameCard.vue
@@ -2,7 +2,7 @@
   <div class="wrappen">
       <div class="GameComponentPopular">
       <div class="ImagePopular">
-        <img class="ImageComponentPopular" v-bind:src="require('@/assets/img/' + Game.image)" wigth="255" height="324">
+        <img class="ImageComponentPopular" v-bind:src="Game.image" wigth="255" height="324">
       </div>
         <div class="RatingAndTitlePopular">
           <router-link :to="{ name: 'singlePage', params: { id: Game.id } }">
@@ -11,9 +11,7 @@
           <p class="PricePopular">{{Game.price}}</p>
         </div>
         <p class="UnderNamePopular">{{Game.undername}}</p>
-
-
-</div>
+    </div>
 </div>
 </template>
 
