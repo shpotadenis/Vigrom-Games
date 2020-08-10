3872a661ca07f3df9a19befbd5ccf837d8f96508
Kate <skv.studies@gmail.com>
2020-08-10 15:33:45 +0300

    Реализован вывод рекомендуемых игр.

--- vigames/games/urls.py
+++ vigames/games/urls.py
@@ -3,10 +3,11 @@ from .views import AccountDetail, OutputAllNewsView, GameDetail, OutputGames, \
     GameRatingDetail, BuyGameDetail, WishListDetail, AssessPostDetail, OutputLibrary, DownloadGame, \
     PostView, GameCategoryDetail, CommentNewsCreateView, CommentGameCreateView, FaqDetail, RoleView, \
     QuestionDetail, DownloadMedia, OutputStatistics, OutputWishlist, SearchView, OutputGenreGames, \
-    OutputGenreTopGames, HideGameDetail, ShowGameDetail, GameInfoToEditDetail
+    OutputGenreTopGames, HideGameDetail, ShowGameDetail, GameInfoToEditDetail, RecommendedGamesDetail
 
 urlpatterns = [
     path('main/new', OutputGames.as_view(), name="main"),
+    path('main/recommended', RecommendedGamesDetail.as_view(), name="recommended_games"),
 
     path('profile', AccountDetail.as_view(), name="profile"),
     path('profile/library', OutputLibrary.as_view(), name="library"),
