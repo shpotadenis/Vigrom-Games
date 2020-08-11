f02ad9d6f1fd2797d660b1ab4ba2679c1befaf45
VladislavTrifonov <vtrifonov548@gmail.com>
2020-08-11 10:20:01 +0300

    Страница рекомендаций

--- vigames/frontend/src/pages/HomeGamePage/recommendationgamepage/RecommendationGamePage.js
+++ vigames/frontend/src/pages/HomeGamePage/recommendationgamepage/RecommendationGamePage.js
@@ -1,4 +1,4 @@
-
+import games from '../../../api/modules/games.js'
 import GameCard  from "@/components/GameCard/GameCard.vue"
 
 export default {
@@ -7,113 +7,9 @@ export default {
     GameCard
   },
   data(){
-  return{
-    recommendation:[
-      {
-      id:"1",
-      image:"MaskGroup.svg",
-      name:"DANGER CREW",
-      rating:"4.9",
-      undername:"Ubisoft Nadeo | Ubisoft",
-      price:"299.99",
-      scrin:'vessel-5.jpg',
-      scrin1:'c97a9.jpg',
-      scrin2:'Pic1.jpg',
-      scrin3:'c97a9.jpg',
-      icon:'cart-icon.png'
-      },
-      {
-      id:"2",
-      image:"MaskGroup.svg",
-      name:"DANGER CREW",
-      rating:"4.9",
-      undername:"Ubisoft Nadeo | Ubisoft",
-      price:"299.99",
-      scrin:'vessel-5.jpg',
-      scrin1:'c97a9.jpg',
-      scrin2:'Pic1.jpg',
-      scrin3:'c97a9.jpg',
-      icon:'cart-icon.png'
-      },
-      {
-      id:"3",
-      image:"MaskGroup.svg",
-      name:"DANGER CREW",
-      rating:"4.9",
-      undername:"Ubisoft Nadeo | Ubisoft",
-      price:"299.99",
-      scrin:'vessel-5.jpg',
-      scrin1:'c97a9.jpg',
-      scrin2:'Pic1.jpg',
-      scrin3:'c97a9.jpg',
-      icon:'cart-icon.png'
-      },
-      {
-      id:"4",
-      image:"MaskGroup.svg",
-      name:"DANGER CREW",
-      rating:"4.9",
-      undername:"Ubisoft Nadeo | Ubisoft",
-      price:"299.99",
-      scrin:'vessel-5.jpg',
-      scrin1:'c97a9.jpg',
-      scrin2:'Pic1.jpg',
-      scrin3:'c97a9.jpg',
-      icon:'cart-icon.png'
-      },
-      {
-      id:"5",
-      image:"MaskGroup.svg",
-      name:"DANGER CREW",
-      rating:"4.9",
-      undername:"Ubisoft Nadeo | Ubisoft",
-      price:"299.99",
-      scrin:'vessel-5.jpg',
-      scrin1:'c97a9.jpg',
-      scrin2:'Pic1.jpg',
-      scrin3:'c97a9.jpg',
-      icon:'cart-icon.png'
-      },
-      {
-      id:"6",
-      image:"MaskGroup.svg",
-      name:"DANGER CREW",
-      rating:"4.9",
-      undername:"Ubisoft Nadeo | Ubisoft",
-      price:"299.99",
-      scrin:'vessel-5.jpg',
-      scrin1:'c97a9.jpg',
-      scrin2:'Pic1.jpg',
-      scrin3:'c97a9.jpg',
-      icon:'cart-icon.png'
-      },
-      {
-      id:"7",
-      image:"MaskGroup.svg",
-      name:"DANGER CREW",
-      rating:"4.9",
-      undername:"Ubisoft Nadeo | Ubisoft",
-      price:"299.99",
-      scrin:'vessel-5.jpg',
-      scrin1:'c97a9.jpg',
-      scrin2:'Pic1.jpg',
-      scrin3:'c97a9.jpg',
-      icon:'cart-icon.png'
-      },
-      {
-      id:"8",
-      image:"MaskGroup.svg",
-      name:"DANGER CREW",
-      rating:"4.9",
-      undername:"Ubisoft Nadeo | Ubisoft",
-      price:"299.99",
-      scrin:'vessel-5.jpg',
-      scrin1:'c97a9.jpg',
-      scrin2:'Pic1.jpg',
-      scrin3:'c97a9.jpg',
-      icon:'cart-icon.png'
-      },
-    ]
-  }
-  }
+    return{
+      recommendation:[]
+    }
+  },
+  
 }
