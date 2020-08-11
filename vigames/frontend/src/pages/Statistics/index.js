29f2495a553d28cc6bacfb627f8df20d46657587
Lagumir <Legals92@mail.ru>
2020-08-11 15:27:59 +0300

    статистика

--- vigames/frontend/src/pages/Statistics/index.js
+++ vigames/frontend/src/pages/Statistics/index.js
@@ -1,16 +1,34 @@
 import FooterComponent from '../../components/FooterComponent/index.vue'
+import { Line, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
 
 export default {
+    extends: Line,
+    mixins: [reactiveProp],
+    props: ['options'],
     name: "Statistics",
     components: {
-        FooterComponent
+        FooterComponent,
+
     },
     data(){
-        return{
-        }
+      return {};
     },
     methods:{
     },
     computed:{
+    },
+    chartData:{
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [{
+              label: 'My First dataset',
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [0, 10, 5, 2, 20, 30, 45]
+          }]
+    },
+    mounted () {
+      this.renderChart(this.chartData, this.options)
     }
+
 }
