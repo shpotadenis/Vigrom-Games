cdc37339a2284fa81940f2bff8ba8da8acee6dc2
Alexander Sergeev <Hesss@users.noreply.github.com>
2020-08-11 11:33:10 +0300

    Переделал компонент карточки игры (теперь просто указываешь скидку в переменной sale и выводится как карточка со скидкой)

--- vigames/frontend/src/components/GameCard/GameCard.vue
+++ vigames/frontend/src/components/GameCard/GameCard.vue
@@ -3,14 +3,22 @@
       <div class="GameComponentPopular">
       <div class="ImagePopular">
         <img class="ImageComponentPopular" v-bind:src="Game.image" wigth="255" height="324">
+        <div v-if="Game.sale > 0" class="Sale-block">
+          <div class="Sale-value">СКИДКА {{Game.sale}} %</div>
+        </div>
       </div>
         <div class="RatingAndTitlePopular">
           <router-link :to="{ name: 'singlePage', params: { id: Game.id } }">
               <p class="NameGamePopular">{{Game.name}}</p>
           </router-link>
-          <p class="PricePopular">{{Game.price}}</p>
+          <p v-if="Game.sale == 0" class="PricePopular">{{Game.price}}</p>
+          <p v-if="Game.sale > 0" class="PriceAfter">{{Math.floor(((Game.price * (100-Game.sale))/100)*100)/100}} <img src="@/assets/img/ruble-gold.svg" alt="Ruble"></p>
         </div>
-        <p class="UnderNamePopular">{{Game.undername}}</p>
+
+        <div class="Footer">
+          <p class="UnderNamePopular">{{Game.undername}}</p>
+          <p v-if="Game.sale > 0" class="PriceBefore">{{Game.price}} <img src="@/assets/img/ruble-gray-2.svg" alt="Ruble"></p>
+        </div>
     </div>
 </div>
 </template>
