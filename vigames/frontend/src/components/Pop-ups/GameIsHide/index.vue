77ee7f7c41e9d94d864538291fa66059ed68c13e
Alexander Sergeev <Hesss@users.noreply.github.com>
2020-08-11 15:25:55 +0300

    Добавил поп-ап "Скрыть игру"

---
+++ vigames/frontend/src/components/Pop-ups/GameIsHide/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="popup-wrapper">
+      <div class="container">
+        <game-hide v-if="isBtnClick" @close="isBtnClick = false">  </game-hide>
+        <div class="Header">
+          <div class="head-name">Скрыть игру</div>
+          <div class="closer">
+            <button @click=" $emit('close')" class="close-icon">
+                <img src="@/assets/img/close-outline.svg" alt="Close">
+            </button>
+          </div>
+         </div>
+         <hr>
+
+        <div class="Content">
+          Вы уверены, что хотите скрыть игру “DANGER CREW”? Она будет недоступна
+          для покупки, просмотра и поиска игроками, а также исчезнет из ленты.
+          Возможность просмотра и скачивания сохранится у игроков, купивших игру.
+         </div>
+        <div class="Footer">
+          <button class="Btn" @click="hideGame">Cкрыть</button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script src="./index.js" type="application/javascript">
+</script>
+
+<style scoped src="./index.css" rel="stylesheet">
+</style>
