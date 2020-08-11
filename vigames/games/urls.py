2c4c23a2aee299598b0454e4e8acd61af73e413b
Algasenok <aleksandr.kozlyakov@yandex.ru>
2020-08-11 15:45:34 +0300

    Сделан вывод статистики покупок по дням.

--- vigames/games/urls.py
+++ vigames/games/urls.py
@@ -4,12 +4,13 @@ from .views import AccountDetail, OutputAllNewsView, GameDetail, OutputGames, \
     PostView, GameCategoryDetail, CommentNewsCreateView, CommentGameCreateView, FaqDetail, RoleView, \
     QuestionDetail, DownloadMedia, OutputStatistics, OutputWishlist, SearchView, OutputGenreGames, \
     OutputGenreTopGames, HideGameDetail, ShowGameDetail, GameInfoToEditDetail, RecommendedGamesDetail, \
-    BestGamesDetail
+    BestGamesDetail, PopularGamesDetail
 
 urlpatterns = [
     path('main/new', OutputGames.as_view(), name="main"),
     path('main/recommended', RecommendedGamesDetail.as_view(), name="recommended_games"),
     path('main/best', BestGamesDetail.as_view(), name="best_games"),
+    path('main/popular', PopularGamesDetail.as_view(), name="popular_games"),
 
     path('profile', AccountDetail.as_view(), name="profile"),
     path('profile/library', OutputLibrary.as_view(), name="library"),
--- vigames/games/views.py
+++ vigames/games/views.py
@@ -709,7 +709,10 @@ class RecommendedGamesDetail(ListAPIView):
         return Response(serializer.data)
 
 class BestGamesDetail(APIView):
-    """ Вывод бестселлеров (учитывается количество покупок за последний месяц)"""
+    """
+    Вывод бестселлеров (учитывается количество покупок за последний месяц)
+    Выдается 5 самых покупаемых игр
+    """
 
     def get(self, request):
         game = Game.objects.all()
@@ -725,3 +728,26 @@ class BestGamesDetail(APIView):
             for g in game_dict[j]:
                 serializer.append(OutputShortGameInfoSerializer(g).data)
         return Response(serializer[0:5])
+
+
+class PopularGamesDetail(APIView):
+    """
+    Вывод популярного (учитывается количество просмотров за последний месяц)
+    Выдается 4 самых популярных игры
+    """
+
+    def get(self, request):
+        game = Game.objects.all()
+        game_dict = {}
+        serializer = []
+        for i in game:
+            num = [i.num for i in Views_Game.objects.filter(game=i, date__range=[date.today()-timedelta(days=30), date.today()])]
+            num = sum(num)
+            if num not in game_dict:
+                game_dict[num] = [i, ]
+            else:
+                game_dict[num].append(i)
+        for j in sorted(game_dict.keys(), reverse=True)[0:4]:
+            for g in game_dict[j]:
+                serializer.append(OutputShortGameInfoSerializer(g).data)
+        return Response(serializer[0:4])
